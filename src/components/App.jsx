f50fcef68a2a6947e8ad9f702acc119a0e41f6f9
francis2911 <138940663+francis2911@users.noreply.github.com>
2024-05-09 20:57:48 -0400

    cambios en la ruta

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,35 @@
-export const App = () => {
-  return <div>React homework template</div>;
+import React from 'react';
+
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Navbar from './navbar/Navbar';
+
+import { Home, Movie, MovieDetails } from '../views';
+
+import { Cast } from './cast/Cast';
+
+import { Reviews } from './reviews/Reviews';
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/movies" element={<Movie />} />
+
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </>
+  );
 };
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import { RouterProvider } from 'react-router-dom';
-import Routes from './routes/Routes';
+
+import ReactDOM from 'react-dom';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import App from 'components/App';
+
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={Routes}>
+    <BrowserRouter basename="/goit-react-hw-05-movies">
       <App />
-    </RouterProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
